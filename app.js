23122dd6598aebde9d911be0a3ecb2abd00f1af5
Philippe Ngom <philngom@gmail.com>
2021-11-05 12:22:24 -0700

    finished functionality of calculator

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 // import functions and grab DOM elements
+import { add, subtract, multiply, divide } from './math-utils.js';
+const x = document.querySelector('#x');
+const y = document.querySelector('#y');
+const arithmetic = document.getElementById('drop-down');
+const equals = document.querySelector('#equals');
+const answer = document.querySelector('.answer');
 
 // initialize state
 
-// set event listeners 
+// set event listeners
   // get user input
-  // use user input to update state 
+  // use user input to update state
   // update DOM to reflect the new state
+equals.addEventListener('click', () => {
+  // console.log(arithmetics)
+    if (arithmetic.value === 'add') {
+        answer.textContent = add(+x.value, +y.value);
+    } else if (arithmetic.value === 'subtract') {
+        answer.textContent = subtract(+x.value, +y.value);
+    } else if (arithmetic.value === 'multiply') {
+        answer.textContent = multiply(+x.value, +y.value);
+    } else if (arithmetic.value === 'divide') {
+        answer.textContent = divide(+x.value, +y.value);
+    }
+    // console.log(arithmetic.value(x, y));
+});
+
